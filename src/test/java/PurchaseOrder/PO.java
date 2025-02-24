4c7a50ed446f1528d2f8d09204f478a24983164b
YeshwanthMyc <yeshwanthdhanaraj310@gmail.com>
2025-02-24 12:41:24 +0530

    First commit

---
+++ src/test/java/PurchaseOrder/PO.java
@@ -0,0 +1,60 @@
+package PurchaseOrder;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PO {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.get("http://qualiantracker.dyndns.org:9090/grpmain/security/Login");
+		driver.findElement(By.id("user")).sendKeys("Openbravo");
+		driver.findElement(By.id("password")).sendKeys("12");
+		driver.findElement(By.id("buttonOK")).click();
+		/*
+		 * wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+		 * "//td[contains(text(),'Application')]")));
+		 * driver.findElement(By.xpath("//td[contains(text(),'Application')]")).click();
+		 * wait.until(ExpectedConditions.visibilityOfElementLocated(By.
+		 * xpath("//tr//td//div[.='Procurement Management']")));
+		 * driver.findElement(By.xpath("//tr//td//div[.='Procurement Management']")).
+		 * click();
+		 * 
+		 * List<WebElement> listOfProcurementManagement =
+		 * driver.findElements(By.xpath("//table[@id='isc_53table']//tr"));
+		 * 
+		 * for (WebElement row : listOfProcurementManagement) { WebElement
+		 * transactionsElement =
+		 * row.findElement(By.xpath("//nobr[contains(text(),'Transactions')]"));
+		 * transactionsElement.click(); break; }
+		 */
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr//td//img[@name='isc_Bmain']")));
+		driver.findElement(By.xpath("//tr//td//img[@name='isc_Bmain']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr//td//input[@name='value']")));
+		driver.findElement(By.xpath("//tr//td//input[@name='value']")).sendKeys("Purchase order and contracts summary");
+		Thread.sleep(2000);
+		List<WebElement> list = driver
+				.findElements(By.xpath("//nobr[contains(text(),'Purchase Order and Contracts Summary')]"));
+		for (WebElement row : list) {
+			String PO = row.getText();
+			if (PO.equalsIgnoreCase("Purchase Order and Contracts Summary")) {
+				row.click();
+				break;
+			}
+		}
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[@class='OBToolbarIconButton_icon_newDoc OBToolbarIconButton']")));
+		driver.findElement(By.xpath("//td[@class='OBToolbarIconButton_icon_newDoc OBToolbarIconButton']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table//tr//td//img[@name='isc_14Lmain']")));
+		driver.findElement(By.xpath("//table//tr//td//img[@name='isc_14Lmain']")).click();
+
+	}
+}
